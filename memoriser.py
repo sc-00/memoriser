90816ff032d6e175002afac353b5b9fd329f33cb
sc-00 <sulingchu106@icloud.com>
2025-04-19 09:02:31 +0100

    Add files via upload

--- memoriser.py
+++ memoriser.py
@@ -9,6 +9,8 @@ root.geometry("400x300")
 root.title("memoriser")
 root.config(background = "orange")
 
+files = {}
+
 def openfile():
     file = askopenfile(title = "open")
     listbox1.delete(0,END)
@@ -22,14 +24,28 @@ def savefile():
         print(i.strip(),file = file2)
         listbox1.delete(0,END)
 
+def deletefile():
+    selected_checkbox = listbox1.curselection()
+    for i in selected_checkbox[::-1]:
+        listbox1.delete(i)
+
+def addfile():
+    val = entrybox.get() 
+    listbox1.insert(END,val)
+    entrybox.delete(0,END)
+    messagebox.showinfo("succes","files have been added succesfuly")
+
 saveb = Button(root,text = "SAVE",command = savefile, width = 20)
 saveb.place(x = 265,y = 0)
 
 openb = Button(root,text = "OPEN",command = openfile, width = 20)
 openb.place(x = 2,y = 0)
 
-deleteb = Button(root,text = "DELETE",command = openfile, width = 20)
-deleteb.place(x = 135,y = 0)
+deleteb = Button(root,text = "DELETE",command = deletefile, width = 20)
+deleteb.place(x = 133,y = 0)
+
+addb = Button(root,text = "ADD",command = addfile, width = 20)
+addb.place(x = 265,y = 30)
 
 listbox1 = Listbox(root, width = 62, height = 11)
 for i in range(1,10):
